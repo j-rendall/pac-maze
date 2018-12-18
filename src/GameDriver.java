21abf78c1a37e52ad420701d13c4b18ee5fef1a6
Jack Rendall <jack.c.rendall0@gmail.com>
2018-12-18 13:00:31 -0500

    Merged all work together

--- src/GameDriver.java
+++ src/GameDriver.java
@@ -1,39 +1,46 @@
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import javax.swing.*;
-public class GameDriver extends JPanel {
-    private int x = 20; //x and y coordinates of the ball
-    private int y = 20;
-    private int xa = 100; //Horizontal speed
-    private int ya = 50; //Vertical speed
-    mainMenu mainMenu = new mainMenu();
-//    Building[] list = new Building[5];
-//    UFO ufo = new UFO(this, 50, 50, 0, 0);
-//    Car car = new Car(this, 0, 825, 5);
-/*    public GameDriver() {
-        //Randomly create each thing in the list
-        list[0] = new Building(300, 25, 3, 10, 0.5);
-        list[1] = new Building(450,200, 5, 7, 0.7);
-        list[2] = new Building(300,475, 6, 10, 0.5);
-        list[3] = new Building(400,800, 4, 8, 0.8);
-        list[4] = new Building(550,1025, 3, 5, 0.3);
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.InputStream;
 
-        addKeyListener(new KeyListener() {
+public class GameDriver extends JPanel {
+    private int levelX = 0;
+    private int levelY = 100;
+    private int levelW = 50;
+    private int levelH = 50;
+    private int clickX = 0;
+    private int clickY = 0;
+    mainMenu mainMenu = new mainMenu();
+    private Level level;
+
+
+    public GameDriver(InputStream s) throws Exception {
+        this.level = new Level(s);
+        this.addMouseListener(new MouseListener() {
             @Override
-            public void keyTyped(KeyEvent e) {
+            public void mouseClicked(MouseEvent mouseEvent) {
+                clickX = mouseEvent.getX();
+                clickY = mouseEvent.getY();
             }
+
             @Override
-            public void keyReleased(KeyEvent e) {
-                ufo.keyReleased(e);
+            public void mousePressed(MouseEvent mouseEvent) {
             }
+
             @Override
-            public void keyPressed(KeyEvent e) {
-                ufo.keyPressed(e);
+            public void mouseReleased(MouseEvent mouseEvent) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {
             }
         });
-        setFocusable(true);
-    }*/
+    }
 /*    private void move() {
         if (x + xa < 0) //If the ball exceeds the left
             xa = -1*xa; //go right
@@ -58,27 +65,33 @@ public class GameDriver extends JPanel {
 
         mainMenu.paint(g2d);
 
-//        for (Building b:list)
-//            b.paint(g2d);
-//
-//        ufo.paint(g2d);
-//
-//        car.paint(g2d);
+        if (mainMenu.play) {
+            this.level.drawOn(g2d, this.levelX, this.levelY, this.levelW, this.levelH);
+        }
+            Level.Point point = this.level.getCellIndex(this.levelX, this.levelY, this.levelW, this.levelH, this.clickX, this.clickY);
+            g2d.setColor(Color.GREEN);
+            //g2d.drawString(point.toString(), 100, 100);   //draws coordinate of click
     }
-    public static void main(String[] args) throws InterruptedException
-    {
-        JFrame f = new JFrame("Project MAZE (Alpha 1.0)");
-        GameDriver gd = new GameDriver();
-        f.add(gd);
-        f.setSize(750,800);
-        f.setVisible(true);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        while (true)
-        {
-            //gd.move(); //Updates the coordinates
-            gd.repaint(); //Calls the paint method
-            Thread.sleep(10); //Pauses for a moment
+    public static void main(String[] args) throws InterruptedException {
+        JFrame f = new JFrame("Project MAZE (Alpha 1.0)");
+        try {
+            int num =5;
+
+            GameDriver gd = new GameDriver(GameDriver.class.getResourceAsStream("/level 1.txt"));
+            f.add(gd);
+            f.setSize(766, 890);
+            f.setVisible(true);
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            while (true) {
+                //gd.move(); //Updates the coordinates
+                gd.repaint(); //Calls the paint method
+                Thread.sleep(10); //Pauses for a moment
+            }
+        }
+        catch (Exception ex) {
+            throw new RuntimeException(ex);
         }
     }
 }
