9e20650ff14629dffc3be2a10bd9a678e9d68a89
Jack Rendall <jack.c.rendall0@gmail.com>
2019-01-11 14:06:19 -0500

    added resources

---
+++ src/newstuff/Resources.java
@@ -0,0 +1,26 @@
+package newstuff;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+
+public class Resources {
+    public static final Image logo = loadImage("pacmanlogo.png");
+    public static final Image settingsimg = loadImage("settings.jpg");
+    public static final Image instructionsimg = loadImage("instructions.jpg");
+    public static final Image pac1 = loadImage("pacman1.png");
+    public static final Image pac2 = loadImage("pacman2.png");
+    public static final Image pac3 = loadImage("pacman3.png");
+    public static final Image pac4 = loadImage("pacman4.png");
+    public static final Image blinky = loadImage("Blinky.png");
+    public static final Image clyde = loadImage("Clyde.png");
+    public static final Image inky = loadImage("Inky.png");
+    public static final Image pinky = loadImage("Pinky.png");
+
+    public static Image loadImage(String filename) {
+        try {
+            return ImageIO.read(Resources.class.getResourceAsStream(filename));
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
